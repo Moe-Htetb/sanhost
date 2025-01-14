9da64471b222ab3aa1e82a2f35bfe09e6e960b85
Moe-Htetb <moehtetoo2005@gmail.com>
2025-01-14 22:03:43 +0630

    second change

--- src/js/intialRender.js
+++ src/js/intialRender.js
@@ -1,7 +1,7 @@
 import { addList } from "./list.js";
 
 const intialRender = () => {
-  const tasks = ["work", "learn-js", "watch movie"];
+  const tasks = ["work", "learn-js", "watch movie", "learn-linux", "learn-cli"];
 
   tasks.forEach((task) => {
     addList(task);
